eefe22b3a26e8e08f3f3be0e7a1c05e1dafae3b8
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-10-06 22:13:48 +0200

    restuctured front-end

--- sources/front-end/src/routes/+layout.svelte
+++ sources/front-end/src/routes/+layout.svelte
@@ -1,9 +1,4 @@
-<script>
-  import Header from '$lib/components/header/Header.svelte';
-  import Footer from '$lib/components/footer/Footer.svelte';
-</script>
-<Header />
-<main>
-  <slot />
-</main>
-<Footer />
+<style>
+</style>
+
+<slot />
