056753fd61f4035ffd9219f48ac6e6430dddaa35
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-10-24 11:44:09 +0200

    fix: font loading issue

--- sources/front-end/src/lib/components/main-banner/MainBanner.svelte
+++ sources/front-end/src/lib/components/main-banner/MainBanner.svelte
@@ -1,1 +1,22 @@
-banner
+<style>
+  div {
+    top: 25%;
+
+    & > h2 {
+      font-size: 4rem;
+    }
+  
+    & > h1 {
+      font-size: 6rem;
+    }
+  }
+</style>
+
+<div>
+  <h2>
+    open for delivery & pick up
+  </h2>
+  <h1>
+    get it while it's hot
+  </h1>
+</div>
